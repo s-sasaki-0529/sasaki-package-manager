24114ff5e19a34b2ec957397ace14ed58dd7698d
s-sasaki-0529 <shingo.sasaki.0529@gmail.com>
2022-08-28 18:11:35 +0900

    package.json から依存関係情報を抜きだす関数を追加

--- src/packageJson.ts
+++ src/packageJson.ts
@@ -1,5 +1,25 @@
+import { readFile } from 'fs/promises'
 import { findUp } from 'find-up'
 
-export async function getPackageJsonPath() {
+export async function findPackageJsonPath() {
   return findUp('package.json')
+}
+
+export async function parsePackageJson(path: string): Promise<PackageJson> {
+  return new Promise(resolve => {
+    readFile(path)
+      .then(data => {
+        const json = JSON.parse(data.toString())
+        resolve({
+          dependencies: json.dependencies,
+          devDependencies: json.devDependencies
+        })
+      })
+      .catch(() => {
+        resolve({
+          dependencies: {},
+          devDependencies: {}
+        })
+      })
+  })
 }
