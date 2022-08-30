7e0dd70c3d5b73194f7f10b562aa781bcc91b91a
s-sasaki-0529 <shingo.sasaki.0529@gmail.com>
2022-08-30 13:05:42 +0900

    インストール成功時のログ関数を追加

--- src/logger.ts
+++ src/logger.ts
@@ -1,3 +1,7 @@
+export function installLog(name: PackageName, version: Version) {
+  console.log(`${name}@${version} installed`)
+}
+
 export function resolveLog(name: PackageName, vc: VersionConstraint, version: Version) {
   console.log(`${name}@${vc} resolve to ${version}`)
 }
