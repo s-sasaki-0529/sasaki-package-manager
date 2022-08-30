5bea5d368ec3c04e881c4526df399e8a28b39896
s-sasaki-0529 <shingo.sasaki.0529@gmail.com>
2022-08-30 18:57:49 +0900

    何故か CLI が終了しないことがあるので確実にプロセスを落とす

--- src/cli.ts
+++ src/cli.ts
@@ -6,12 +6,13 @@ const program = new Command()
 program
   .command('install')
   .argument('[packageNames...]')
-  .action(packageNames => {
+  .action(async packageNames => {
     const options = program.opts()
-    install(packageNames, {
+    await install(packageNames, {
       saveDev: !!options.saveDev,
       production: !!options.production
     })
+    process.exit(0)
   })
 
 program
