f8a43188c9a32cf3064d15365a1a5721c05714f7
s-sasaki-0529 <shingo.sasaki.0529@gmail.com>
2022-09-01 13:21:53 +0900

    package.json に新しい依存関係を書き込む関数を追加

--- src/packageJson.ts
+++ src/packageJson.ts
@@ -1,10 +1,16 @@
-import { readFile } from 'fs/promises'
+import { writeFile, readFile } from 'fs/promises'
 import { findUp } from 'find-up'
 
+/**
+ * package.json を探索し、そのパスを返す
+ */
 export async function findPackageJsonPath() {
   return findUp('package.json')
 }
 
+/**
+ * package.json を parse し、依存関係オブジェクトを返す
+ */
 export async function parsePackageJson(path: string): Promise<PackageDependencyMap> {
   return readFile(path).then(data => {
     const json = JSON.parse(data.toString())
@@ -13,4 +19,20 @@ export async function parsePackageJson(path: string): Promise<PackageDependencyM
       devDependencies: json.devDependencies || {}
     })
   })
+}
+
+/**
+ * package.json を依存関係オブジェクトを用いて書き換える
+ */
+export async function updatePackageJson(path: string, dependencyMap: PackageDependencyMap) {
+  const data = await readFile(path)
+  const currentJson = JSON.parse(data.toString())
+  const newJson = {
+    ...currentJson,
+    dependencies: dependencyMap.dependencies,
+    devDependencies: dependencyMap.devDependencies
+  }
+  if (Object.keys(newJson.dependencies).length === 0) delete newJson.dependencies
+  if (Object.keys(newJson.devDependencies).length === 0) delete newJson.devDependencies
+  return writeFile(path, JSON.stringify(newJson, null, 2))
 }
